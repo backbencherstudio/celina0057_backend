72ed92a57ff084ea4da063986c4c0dcf3e80fd76
Tariq Monowar Hosain <101199109+Tariq-Monowar@users.noreply.github.com>
2025-06-26 10:18:11 +0600

    Delete prisma/migrations directory

--- prisma/migrations/20250603093341_update_prisma/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `adminId` on the `Foods` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Foods" DROP COLUMN "adminId";
